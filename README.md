d794888794b9ff2d742e188440366be401935a31
yusufyaman07 <yamanyusuf00000@gmail.com>
2024-08-03 09:24:07 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 11_cuma_projeleri
---
+++ destiny_website/main.js
@@ -0,0 +1,20 @@
+// videoları ekrana geldiği anda oynatma
+document.addEventListener("DOMContentLoaded", function () {
+  const videos = document.querySelectorAll("video");
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.play();
+        entry.target.addEventListener("ended", () => {
+          entry.target.currentTime = 0;
+          entry.target.play();
+        });
+      } else {
+        entry.target.pause();
+      }
+    });
+  });
+  videos.forEach((video) => {
+    observer.observe(video);
+  });
+});
