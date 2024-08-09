f369982512b3748a4d339ce3458e672f82b7ce52
yusufyaman07 <yamanyusuf00000@gmail.com>
2024-08-09 22:57:56 +0300

    destiny finished

--- destiny_website/main.js
+++ destiny_website/main.js
@@ -1,3 +1,9 @@
+const btn = document.querySelector(".nav-btn");
+const nav = document.querySelector("nav");
+btn.addEventListener("click", () => {
+  nav.classList.toggle("toggle");
+});
+
 // videoları ekrana geldiği anda oynatma
 document.addEventListener("DOMContentLoaded", function () {
   const videos = document.querySelectorAll("video");
