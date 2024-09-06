c7e0b9326646b96df95d710a3321b6215b8b2432
yusufyaman07 <yamanyusuf00000@gmail.com>
2024-09-07 02:28:52 +0300

    x clone start

---
+++ x_clone/js/auth.js
@@ -0,0 +1,69 @@
+import { authEle } from "./ui.js";
+// console.log(authEle);
+
+// Regex: Regex belirli şartları kontrol etmek için sorgu yapılarını içeren koddur.
+// * Enaz 6 karakter,1 küçük harf,1 büyük harf,birde sayı
+const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$/;
+
+// ! Form kontrolü sonrasında hataları render eden fonk.
+
+// ! Auth sayfasında yer alan form gönderildiğinde çalışacak fonk.
+
+const renderWarns = (nameWarning, passWarning) => {
+  // İsim hatası varsa
+  if (nameWarning) {
+    authEle.nameArea.innerHTML = `
+    <p class="warning">${nameWarning} </p> 
+    `;
+  } else {
+    authEle.nameArea.innerHTML = "";
+  }
+  // Şifre Hatası varsa
+  if (passWarning) {
+    authEle.passArea.innerHTML = `
+          <p class="warning">${passWarning} </p>
+`;
+  } else {
+    authEle.passArea.innerHTML = "";
+  }
+};
+
+authEle.loginForm.addEventListener("submit", (e) => {
+  // Form gönderildiğinde sayfa yenilenmesi iptal edildi
+  e.preventDefault();
+  // Formun içerisindeki verilere erişme
+
+  const name = authEle.nameInp.value;
+  const pass = authEle.passwordInp.value;
+  console.log(name, pass);
+
+  let nameWarning = null;
+  let passWarning = null;
+  // Formdaki isim kısımını kontrol et
+  if (!name) {
+    nameWarning = "İsim kısımı boş bırakılamaz ";
+  } else if (name.length < 5) {
+    nameWarning = "İsim 5 karakterden kısa olamaz ";
+  } else {
+    nameWarning = null;
+  }
+  // Formdaki şifre kısımını kontrol et
+
+  if (!pass) {
+    passWarning = "Şifre giriniz !! ";
+  } else if (pass.length < 6) {
+    passWarning = "Şifre 6 haneden kısa olamaz";
+  } else if (!regex.test(pass)) {
+    passWarning = "Zayıf Şifre";
+  } else {
+    passWarning = null;
+  }
+
+  renderWarns(nameWarning, passWarning);
+
+  if (!nameWarning && !passWarning) {
+    // Beni ana sayfaya yönlendir
+    // Eğer tüm şartlar sağlanıyorsa beni ana sayfaya yönlendir.
+    window.location = "/";
+  }
+});
---
+++ x_clone/js/ui.js
@@ -0,0 +1,7 @@
+export const authEle = {
+  loginForm: document.querySelector("#login"),
+  nameInp: document.querySelector("#name"),
+  passwordInp: document.querySelector("#password"),
+  nameArea: document.querySelector(".name-warning"),
+  passArea: document.querySelector(".pass-warning"),
+};
